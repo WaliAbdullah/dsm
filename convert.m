90027b8424890a38700d20156be3843c9268c691
Wali Mohammad Abdullah <w.abdullah@uleth.ca>
2020-01-27 10:43:02 -0700

    adding files

--- convert.m
+++ convert.m
@@ -10,14 +10,14 @@
 %F=E';
 %status = odswrite ('list.ods', F,'T15');
 
-load T25_out; 
-F=E';
-status = odswrite ('list.ods', F,'T25');
+%load T25_out; 
+%F=E';
+%status = odswrite ('list.ods', F,'T25');
 
 %load T50_out; 
 %F=E';
 %status = odswrite ('list.ods', F,'T50');
 
-%load T100_out; 
-%F=E';
-%status = odswrite ('list.ods', F,'T100');
+load T100_out; 
+F=E';
+status = odswrite ('list.ods', F,'T100');
